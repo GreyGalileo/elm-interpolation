6d9e728f295f7900900112bd5874b1ee0f6c510d
Galileo Grey <galileo.e.grey@gmail.com>
2024-01-19 22:36:12 +0100

    Finished programming initial naive lagrange polynomial and showing graph on screen with svg

--- main.elm
+++ main.elm
@@ -1,11 +1,14 @@
 import Browser
 import Browser.Events as E
+import Browser.Dom as Dom
+import Task
 import Json.Decode as D
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (style)
-import Element
 import Html.Events.Extra.Mouse as Mouse
+import Svg
+import Svg.Attributes as Svga
 
 
 -- MAIN
@@ -30,13 +33,14 @@ type alias Point =
 
 type alias Model =
   { points: List Point
+  ,  svg_width: Float
   }
 
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Model []
-  , Cmd.none
+  ( Model [] 0
+  , getSvgWidth
   )
 
 
@@ -47,20 +51,29 @@ init _ =
 type Msg
   = NewPoint Point
   | Clear
+  |SvgWidth (Result Dom.Error Dom.Element)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Clear -> (
-      Model ([])
+      Model [] model.svg_width
       ,Cmd.none
       )
 
     NewPoint point ->
-      ( Model (point::model.points)
+      ( Model (point::model.points) model.svg_width
       , Cmd.none
       )
+    
+    SvgWidth result ->
+      case result of
+        Ok elem ->
+          (Model model.points elem.element.width, Cmd.none)
+        Err e ->
+          (model, Cmd.none)
+        
 
 
 
@@ -69,28 +82,66 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
+getSvgWidth: Cmd Msg
+getSvgWidth =
+  Task.attempt SvgWidth (Dom.getElement "Graphs")
 
 -- VIEW
 point_to_str: Point -> String
 point_to_str (x,y) =
-  "(" ++ String.fromFloat x ++ ", " ++ String.fromFloat y ++ ") "
+  String.fromFloat x ++ "," ++ String.fromFloat y ++ " "
  
-
 list_points : List Point -> String
 list_points list =
   (List.map point_to_str list) |> List.foldl (++) "" 
 
+
+point_to_svg: Point -> Svg.Svg msg
+point_to_svg (x,y) = 
+  Svg.circle [ 
+    Svga.cx (String.fromFloat x),
+    Svga.cy (String.fromFloat y),
+    Svga.r "2", Svga.fill "black" ] []
+
+build_li_aux: Float -> Point -> (Float->Float) -> (Float -> Float)
+build_li_aux xi (x1,y1) li=
+  if xi == x1 then (li) else
+  (\x -> (x - x1) / (xi - x1) * (li x))
+  
+build_li: Float -> List Point -> (Float -> Float)
+build_li xi points =
+  List.foldl (build_li_aux xi) (\x -> 1) points
+
+build_lagrange: List Point -> (Float -> Float)
+build_lagrange points =
+  List.foldl (\(xi,yi) f -> (\x ->  (f x) + yi * (build_li xi points) x)) (\x -> 0.0) points
+
+
+divided_differences: List Point -> Float -> Float
+divided_differences points x =
+  0.0
+
+
 view : Model -> Html Msg
 view model =
+  let lagrange: (Float -> Float) 
+      lagrange = build_lagrange model.points in
+  let path_string = (List.range 0 (ceiling model.svg_width) |> List.map (\a -> (toFloat a)) |> List.map (\x -> (x , lagrange x))|> list_points) 
+  in
   div 
     [
-    Mouse.onDown (\n -> NewPoint n.screenPos),
-    style "background-color" "yellow",
-    style "height" fill,
-    style "width" fill
+    Mouse.onDown (\n -> NewPoint n.clientPos),
+    style "background-color" "orange",
+    style "height" "100vh",
+    style "width" "100vw",
+    style "margin" "0px"
     ]
-
-    [ h1 [] [ text ("Click anywhere") ],
-    p [] [text (list_points model.points)],
-    button [onClick (Clear) ] [text ("Clear Points")]
+    [
+    --p [style "margin" "0px"] [text (list_points model.points)],
+    Svg.svg[Svga.width "100%", Svga.height "94%", Svga.id "Graphs"]
+    ([
+      Svg.text "Click Anywhere",
+      Svg.polyline [ Svga.fill "none", Svga.stroke "pink", Svga.points path_string ] []
+    ] ++ (List.map point_to_svg model.points)) ,
+    button [onClick (Clear) , style "height" "5%", style "margin" "0px"] [text ("Clear Points")]
     ]
